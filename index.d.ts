84bef09877db5f6ace3a95e684addd979fff1875
Terry O'Neill <terry.oneill@trystal.net>
2016-07-12 15:44:41 -0600

    first types that reference/extend immutable

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,5 @@
 declare module "@trystal/interfaces" {
+    import {Map,List} from 'immutable'
     export interface Payload {
         id: string
         format?: string
@@ -22,5 +23,14 @@ declare module "@trystal/interfaces" {
     // thes support immutablejs core for trists
     export type PayloadPropName = 'id' | 'trystup' | 'format'
     export type NodePropName = 'id' | 'rlevel' | 'prev' | 'next' | 'PV' | 'NV' | 'payload';
+
+    export interface PayloadIM extends Map<PayloadPropName, string> { 
+    toJS(): Payload;
+    }
+    export interface NodeIM extends Map<NodePropName, PayloadIM | string | number> {
+        toJS(): Node;
+    }
+    export interface ChainIM extends Map<string, NodeIM> {}
+    export interface IDListIM extends List<string> {}
 
 }
